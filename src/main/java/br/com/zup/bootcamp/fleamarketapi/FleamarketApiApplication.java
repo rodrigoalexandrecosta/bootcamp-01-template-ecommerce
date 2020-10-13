03e1fad1a33cf562695c20a6280ecad09f1b9248
Rodrigo Alexandre Costa <rodrigoalexandrecosta@gmail.com>
2020-10-13 16:00:38 -0300

    Add api basic skeleton

---
+++ src/main/java/br/com/zup/bootcamp/fleamarketapi/FleamarketApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.bootcamp.fleamarketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleamarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleamarketApiApplication.class, args);
+	}
+
+}
